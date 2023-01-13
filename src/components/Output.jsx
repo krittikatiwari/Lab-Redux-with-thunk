5a129a1869f538d3fabef1c1673ec19a1299e41d
krittikatiwari <113347197+krittikatiwari@users.noreply.github.com>
2023-01-13 14:51:56 +0530

    Add files via upload

---
+++ src/components/Output.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export default function Output() {
+  return (
+    <div className='center' >
+        <Link to={'/list'} >
+            <button>
+                Fetch Data
+            </button>
+        
+        </Link>
+    </div>
+  )
+}
